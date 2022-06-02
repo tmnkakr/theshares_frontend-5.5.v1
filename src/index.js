355500ef2afc4d7fb9e1053586603303503da627
tmnkakr <tmnkakr@gmail.com>
2022-06-02 12:53:59 +0530

    comit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './app'
 ReactDom.render(
     <BrowserRouter>
-    bjxh
+    <App />
     </BrowserRouter>,
     document.getElementById('root')
 )
